2883130ce4d5cce1b51c9ba5bb09da733faca42b
lrtinkiv <lrtinkiv@jyu.fi>
2024-01-08 19:21:21 +0200

    small fixes

--- mailProtocols/SMTPserver/Program.cs
+++ mailProtocols/SMTPserver/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
@@ -6,12 +7,11 @@ class SmtpServer
 {
     static void Main(string[] args)
     {
-        // Local IP adress and port 1025
+        // Local IP address and port 1025
         IPEndPoint iep_smtp = new IPEndPoint(IPAddress.Loopback, 1025);
-        Console.WriteLine("SMTP endpoint : " + iep_smtp);
+        Console.WriteLine("SMTP endpoint: " + iep_smtp);
 
         StartSMTPserver(iep_smtp);
-
     }
 
     private static void StartSMTPserver(IPEndPoint iep_smtp)
@@ -20,42 +20,44 @@ class SmtpServer
         server.Bind(iep_smtp);
         server.Listen(1);
         Console.WriteLine("SMTP server started.");
-        listenSMTP(server, iep_smtp);
-        Console.ReadKey();
-        server.Close();
 
-
-    }
-
-    private static void listenSMTP(Socket server, IPEndPoint iep_smtp)
-    {
-        while(true)
+        while (true)
         {
             Socket client = server.Accept();
             string client_ip = ((IPEndPoint)(client.RemoteEndPoint)).Address.ToString();
             string client_port = ((IPEndPoint)(client.RemoteEndPoint)).Port.ToString();
 
-            client.Send(Encoding.UTF8.GetBytes("220"+ iep_smtp +" ESMTP Postfix\n"));
+            client.Send(Encoding.UTF8.GetBytes("220 " + iep_smtp + " ESMTP Postfix\n"));
 
+            Boolean conversation = true;
             string from = "";
             string to = "";
             StringBuilder data = new StringBuilder();
 
-            while(true)
+            while (conversation)
             {
                 byte[] buffer = new byte[2048];
-                client.Receive(buffer);
-                string client_msg = Encoding.UTF8.GetString(buffer);
-                Console.WriteLine("[{0}:{1}]: {2}",client_ip,client_port,client_msg);
+                int bytesRead = client.Receive(buffer);
+                if (bytesRead == 0)
+                {
+                    // Connection closed by the client
+                    client.Close();
+                    break;
+                }
+
+                string client_msg = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("[{0}:{1}]: {2}", client_ip, client_port, client_msg);
+
                 string[] lines = client_msg.Split(
                     new[] { "\r\n", "\r", "\n" },
-                        StringSplitOptions.None
+                    StringSplitOptions.None
                 );
+
                 foreach (string line in lines)
                 {
                     if (line.StartsWith("HELO"))
                     {
-                        client.Send(Encoding.UTF8.GetBytes("250 Hello " +client_ip+", I am glad to meet you\r\n"));
+                        client.Send(Encoding.UTF8.GetBytes("250 Hello " + client_ip + ", I am glad to meet you\r\n"));
                     }
                     else if (line.StartsWith("MAIL FROM:"))
                     {
@@ -73,18 +75,18 @@ class SmtpServer
                         while (true)
                         {
                             byte[] dataBuffer = new byte[2048];
-                            int bytesRead = client.Receive(dataBuffer);
+                            int dataBytesRead = client.Receive(dataBuffer);
 
-                            if (bytesRead == 0)
+                            if (dataBytesRead == 0)
                             {
                                 // Connection closed by the client
                                 client.Close();
+                                conversation = false;
                                 break;
                             }
 
-                            string dataLine = Encoding.UTF8.GetString(dataBuffer, 0, bytesRead);
+                            string dataLine = Encoding.UTF8.GetString(dataBuffer, 0, dataBytesRead);
                             data.Append(dataLine);
-                            Console.WriteLine(dataLine);
 
                             if (dataLine == ".\r\n")
                             {
@@ -96,12 +98,12 @@ class SmtpServer
                     else if (line.ToUpper() == "QUIT")
                     {
                         client.Send(Encoding.UTF8.GetBytes("221 Bye\r\n"));
-                        break;
+                        client.Close();
+                        conversation = false;
+                        break;  // Break from the inner loop, but stay in the outer loop to accept new clients
                     }
                 }
             }
         }
     }
-
 }
-
