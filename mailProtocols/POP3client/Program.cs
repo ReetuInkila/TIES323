bcce7f3159ac71e81a8046d83903f4b5a93d988e
lrtinkiv <lrtinkiv@jyu.fi>
2024-01-10 08:34:27 +0200

    main send predefined pop3 commands

--- mailProtocols/POP3client/Program.cs
+++ mailProtocols/POP3client/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+using System.Net.Security;
+using System.Text;
+using System.Text.Json;
+
+class Credentials
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
+
+class Pop3Client
+{
+    static void Main(string[] args)
+    {
+        /// Read credentials from the configuration file
+        var jsonString = File.ReadAllText("secrets.json");
+        Credentials credentials = JsonSerializer.Deserialize<Credentials>(jsonString);
+
+        string pop3Server = "127.0.0.1";
+        int pop3Port = 110; // Default POP3 port
+
+        using (TcpClient client = new TcpClient(pop3Server, pop3Port))
+        {
+            using (Stream stream = client.GetStream())
+            using (StreamReader reader = new StreamReader(stream, Encoding.ASCII))
+            using (StreamWriter writer = new StreamWriter(stream, Encoding.ASCII))
+            {
+                // Read the welcome message from the server
+                Console.WriteLine(reader.ReadLine());
+
+                // Send USER command
+                SendCommand(writer, "USER "+credentials.Username);
+
+                // Read the response for USER command
+                Console.WriteLine(reader.ReadLine());
+
+                // Send PASS command
+                SendCommand(writer, "PASS "+credentials.Password);
+
+                // Read the response for PASS command
+                Console.WriteLine(reader.ReadLine());
+
+                // Send LIST command
+                SendCommand(writer, "LIST");
+
+                // Read the response for LIST command
+                Console.WriteLine(reader.ReadLine());
+
+                // Send QUIT command
+                SendCommand(writer, "QUIT");
+
+                // Read the response for QUIT command
+                Console.WriteLine(reader.ReadLine());
+            }
+        }
+    }
+
+    static void SendCommand(StreamWriter writer, string command)
+    {
+        writer.WriteLine(command);
+        writer.Flush();
+    }
+}
+
